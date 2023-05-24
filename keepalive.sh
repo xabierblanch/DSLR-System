7dbc2a58d9a5d951f8432da67f564c000edbd9cb
Xabier Blanch Górriz <37353398+xabierblanch@users.noreply.github.com>
2023-05-24 11:21:40 +0200

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# -*- ENCODING: UTF-8 -*-
+'''
+This code is responsible for activating the relay connected to pin 23 of the Raspberry Pi.
+The activation of this relay connects GPIO4 with GND in the WittyPi, allowing the automatic system shutdown to be disabled.
+Thanks to this, the system remains powered on.
+To activate it, execute "sh keepalive.sh" in the command line.
+To shutdown the system, simply activate "sh shutdown.sh".
+'''
 
 gpio -g mode 23 out
 gpio -g write 23 1
